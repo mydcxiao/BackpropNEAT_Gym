fix backprop eval bug

domain/task_gym.py
@@ -70,10 +70,16 @@ class GymTask():
       return fitness
     else:
       wVec = np.where(np.isnan(wVec), 0, wVec)
-      for iRep in range(nRep):
-        reward, wVec = self.testInd(wVec, aVec, view=view, seed=seed+iRep, backprop=backprop, step_size=step_size, backprop_eval=backprop_eval)
-        print(f'Epoch:{iRep}:',reward)
-      return reward, wVec
+      if not backprop_eval:
+        for iRep in range(nRep):
+          reward, wVec = self.testInd(wVec, aVec, view=view, seed=seed+iRep, backprop=backprop, step_size=step_size, backprop_eval=backprop_eval)
+          # print(f'Epoch:{iRep}:',reward)
+        return reward, wVec
+      else:
+        reward = np.empty(nRep)
+        for iRep in range(nRep):
+          reward[iRep] = self.testInd(wVec, aVec, view=view, seed=seed+iRep, backprop=backprop, step_size=step_size, backprop_eval=backprop_eval)
+        return np.mean(reward)
         
 
   def testInd(self, wVec, aVec, view=False,seed=-1, backprop=False, step_size=0.01, backprop_eval=False):
@@ -148,7 +154,7 @@ class GymTask():
         # loss = -np.mean(y * np.log(action + eps) + (1 - y) * np.log(1 - action +eps))
         error = np.sum(np.abs(pred - y))
         nConn = np.count_nonzero(wVec)
-        totalReward = -loss * np.sqrt(1+connPenalty * nConn)
+        totalReward = -error * np.sqrt(1+connPenalty * nConn)
         return totalReward
       
       else:
@@ -199,7 +205,7 @@ class GymTask():
             state = self.env.trainSet
             y = self.env.target
             wVec_np = device_get(wVec).copy()
-            nConn = int(np.count_nonzero(wVec))
+            nConn = np.count_nonzero(wVec_np)
             annOut = act(wVec_np, aVec, self.nInput, self.nOutput, state, False, nNodes)
             action = selectAct(annOut, self.actSelect, False)
             pred = np.where(action > 0.5, 1, 0).reshape(-1, 1)

neat_src/_variation.py
@@ -61,26 +61,27 @@ def recombine(self, species, innov, gen):
   for i in range(nElites):
     children.append(pop[i])
     nOffspring -= 1
-
-  # Get parent pairs via tournament selection
-  # -- As individuals are sorted by fitness, index comparison is 
-  # enough. In the case of ties the first individual wins
-  parentA = np.random.randint(len(pop),size=(nOffspring,p['select_tournSize']))
-  parentB = np.random.randint(len(pop),size=(nOffspring,p['select_tournSize']))
-  parents = np.vstack( (np.min(parentA,1), np.min(parentB,1) ) )
-  parents = np.sort(parents,axis=0) # Higher fitness parent first    
   
-  # Breed child population
-  for i in range(nOffspring):  
-    if np.random.rand() > p['prob_crossover']:
-      # Mutation only: take only highest fit parent
-      child, innov = pop[parents[0,i]].createChild(p,innov,gen)
-    else:
-      # Crossover
-      child, innov = pop[parents[0,i]].createChild(p,innov,gen,\
-                mate=pop[parents[1,i]])
+  if nOffspring > 0: #TODO: why is this necessary?
+    # Get parent pairs via tournament selection
+    # -- As individuals are sorted by fitness, index comparison is 
+    # enough. In the case of ties the first individual wins
+    parentA = np.random.randint(len(pop),size=(nOffspring,p['select_tournSize']))
+    parentB = np.random.randint(len(pop),size=(nOffspring,p['select_tournSize']))
+    parents = np.vstack( (np.min(parentA,1), np.min(parentB,1) ) )
+    parents = np.sort(parents,axis=0) # Higher fitness parent first    
+    
+    # Breed child population
+    for i in range(nOffspring):  
+      if np.random.rand() > p['prob_crossover']:
+        # Mutation only: take only highest fit parent
+        child, innov = pop[parents[0,i]].createChild(p,innov,gen)
+      else:
+        # Crossover
+        child, innov = pop[parents[0,i]].createChild(p,innov,gen,\
+                  mate=pop[parents[1,i]])
 
-    child.express()
-    children.append(child)      
+      child.express()
+      children.append(child)      
 
   return children, innov